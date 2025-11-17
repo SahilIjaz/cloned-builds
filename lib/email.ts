a95954741bcd03fd2b5c428960d4637d3ac9afc4
SahilIjaz <hssahil2913@gmail.com>
2025-11-17 14:22:15 +0500

    fixes made

--- lib/email.ts
+++ lib/email.ts
@@ -2,6 +2,7 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+
 export async function sendOTPEmail(email: string, otp: string, username: string) {
   try {
     const { data, error } = await resend.emails.send({
